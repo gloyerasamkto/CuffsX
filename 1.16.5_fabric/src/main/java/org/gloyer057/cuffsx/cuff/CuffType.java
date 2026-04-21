e8714e093a485e63cf695e38198dd74f6ccac91b
gloyer057 <gloyer777@gmail.com>
2026-04-22 01:23:27 +0300

    Delete 1.16.5_fabric directory

--- 1.16.5_fabric/src/main/java/org/gloyer057/cuffsx/cuff/CuffType.java
+++
@@ -1,8 +0,0 @@
-package org.gloyer057.cuffsx.cuff;
-
-public enum CuffType {
-    HANDS, LEGS;
-
-    public String toNbt() { return name(); }
-    public static CuffType fromNbt(String s) { return valueOf(s); }
-}
